ad18049a58efbc9a9bd3ec489dcd9134dd4fb664
TeaSpillStudios <teaspillstudios@proton.me>
2022-11-03 20:26:15 +0000

    Remove capabilities issues

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -61,7 +61,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp')
 
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 -- require('lspconfig').<SERVER-HERE>.setup { capabilities = capabilities }
@@ -78,4 +78,4 @@ require('lspconfig').sumneko_lua.setup {
   }
 }
 
-require('lspconfig').rust_analyzer.setup { capabilities = capabilities }
+require('lspconfig').rust_analyzer.setup { }
