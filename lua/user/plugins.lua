045d57177dcbdc1bbde89402c5fa125cef39f9b9
TeaSpillStudios <teaspillstudios@proton.me>
2022-12-09 14:40:23 +0000

    Fix indentation

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -13,24 +13,24 @@ end
 local packer_bootstrap = ensure_packer()
 
 require("packer").startup(function(use)
-  -- Packer can manage itself
-  use("wbthomason/packer.nvim")
+    -- Packer can manage itself
+    use("wbthomason/packer.nvim")
 
-  -- Collection of common configurations for the Nvim LSP client
-  use("neovim/nvim-lspconfig")
+    -- Collection of common configurations for the Nvim LSP client
+    use("neovim/nvim-lspconfig")
 
-  -- Visualize LSP progress
-  use({
+    -- Visualize LSP progress
+    use({
     "j-hui/fidget.nvim",
     config = function()
-      require("fidget").setup()
+    require("fidget").setup()
     end
-  })
+    })
 
-  -- Autocompletion framework
-  use("hrsh7th/nvim-cmp")
+    -- Autocompletion framework
+    use("hrsh7th/nvim-cmp")
 
-  use({
+    use({
     -- CMP LSP completion
     "hrsh7th/cmp-nvim-lsp",
     -- CMP nippet completion
@@ -40,58 +40,58 @@ require("packer").startup(function(use)
     "hrsh7th/cmp-buffer",
     after = { "hrsh7th/nvim-cmp" },
     requires = { "hrsh7th/nvim-cmp" },
-  })
+    })
 
-  -- Snippet engine
-  use('hrsh7th/vim-vsnip')
+    -- Snippet engine
+    use('hrsh7th/vim-vsnip')
 
-  -- Adds extra functionality over rust analyzer
-  use("simrat39/rust-tools.nvim")
+    -- Adds extra functionality over rust analyzer
+    use("simrat39/rust-tools.nvim")
 
-  -- Optional
-  use("nvim-lua/popup.nvim")
-  use("nvim-lua/plenary.nvim")
-  use("nvim-telescope/telescope.nvim")
+    -- Optional
+    use("nvim-lua/popup.nvim")
+    use("nvim-lua/plenary.nvim")
+    use("nvim-telescope/telescope.nvim")
 
-  -- Some color scheme other then default
-	use 'shaunsingh/nord.nvim'
+    -- Some color scheme other then default
+    use 'shaunsingh/nord.nvim'
 
-	-- Add a tab bar
-	use 'nvim-tree/nvim-web-devicons'
-	use {'romgrk/barbar.nvim', wants = 'nvim-web-devicons'}
+    -- Add a tab bar
+    use 'nvim-tree/nvim-web-devicons'
+    use {'romgrk/barbar.nvim', wants = 'nvim-web-devicons'}
 
-	-- Add a status bar
-	use {
-	  'nvim-lualine/lualine.nvim',
-  	requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-	}
+    -- Add a status bar
+    use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
 
-	-- Add a panel to view tags
-	use 'preservim/tagbar'
+    -- Add a panel to view tags
+    use 'preservim/tagbar'
 
-	-- Add a startup panel
-	use 'goolord/alpha-nvim'
+    -- Add a startup panel
+    use 'goolord/alpha-nvim'
 
-	-- Add automatic pairing
-	use { 'windwp/nvim-autopairs', config = function() require("nvim-autopairs").setup {} end }
-	
-	-- Open LazyGit inside of NeoVim
-	use "kdheepak/lazygit.nvim"
+    -- Add automatic pairing
+    use { 'windwp/nvim-autopairs', config = function() require("nvim-autopairs").setup {} end }
 
-	-- Keybindings in popup
-	use "liuchengxu/vim-which-key"
+    -- Open LazyGit inside of NeoVim
+    use "kdheepak/lazygit.nvim"
 
-	-- Trouble panel
-	use { 'folke/trouble.nvim', requires = 'kyazdani42/nvim-web-devicons', config = function() require('trouble').setup {} end }
+    -- Keybindings in popup
+    use "liuchengxu/vim-which-key"
 
-	-- Add modifications of a git repository signs
-	use "airblade/vim-gitgutter"
+    -- Trouble panel
+    use { 'folke/trouble.nvim', requires = 'kyazdani42/nvim-web-devicons', config = function() require('trouble').setup {} end }
 
-	-- Add a tree for undos
-	use "sanfusu/neovim-undotree"
+    -- Add modifications of a git repository signs
+    use "airblade/vim-gitgutter"
 
-	-- Add a plugin to comment things out quickly
-	use "tpope/vim-commentary"
+    -- Add a tree for undos
+    use "sanfusu/neovim-undotree"
+
+    -- Add a plugin to comment things out quickly
+    use "tpope/vim-commentary"
 end)
 
 -- the first run will install packer and our plugins
