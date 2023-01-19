c916bdc910218d7c456f6e07f7fde7d97fc7b088
TeaSpillStudios <teaspillstudios@proton.me>
2023-01-19 22:37:46 +0000

    Enable folding

--- lua/prefs.lua
+++ lua/prefs.lua
@@ -5,4 +5,8 @@ require('onedark').load()
 
 vim.cmd("set rnu nu")
 vim.cmd("set clipboard+=unnamedplus")
+
+vim.cmd("set foldmethod=expr")
+vim.cmd("set foldexpr=nvim_treesitter#foldexpr()")
+
 vim.g.mapleader = " "
