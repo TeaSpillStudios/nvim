564b813ac9a1c6ec771d5c5c18e4b6eeaab3601a
TeaSpillStudios <teaspillstudios@proton.me>
2023-01-19 22:48:29 +0000

    Change tab functionality

--- lua/prefs.lua
+++ lua/prefs.lua
@@ -9,4 +9,6 @@ vim.cmd("set clipboard+=unnamedplus")
 vim.cmd("set foldmethod=expr")
 vim.cmd("set foldexpr=nvim_treesitter#foldexpr()")
 
+vim.cmd("set shiftwidth=4 tabstop=4 softtabstop=4 smarttab")
+
 vim.g.mapleader = " "
